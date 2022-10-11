8825f2f425a1bd82ca144c03956175fe71c95c9f
guillaume <guillaume.jean@edu.ece.fr>
2022-10-11 12:27:04 +0200

    a revoir

---
+++ tp4web/src/monsite/containers/nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../index.css';
+
+export default class App extends React.Component {
+    render(){
+        return(
+        <div id="navbar">
+            <button>jeanne</button>
+            <button>martine</button>
+            <button>claude</button>
+        </div>
+
+    )
+
+    }
+}
+
---
+++ tp4web/src/monsite/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../index.css';
+
+export default class App extends React.Component {
+
+    this.state={}
+    render(){
+        return(
+            <nav users={this.state}/>
+
+        )
+
+    }
+}
+
